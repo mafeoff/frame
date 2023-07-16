11b5ab04b02957a9ea9be09f855d8ce2b495fd1d
Maria Fernanda <97399719+mafeoff@users.noreply.github.com>
2023-07-16 15:39:54 -0300

    Add files via upload

---
+++ Maria Fernanda/src/Components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Feito com ❣️ por Maria Fernanda Ferreira  © 2023
+        </footer>
+    )
+}
+
+export default Footer
